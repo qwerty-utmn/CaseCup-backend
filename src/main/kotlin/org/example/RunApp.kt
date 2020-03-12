bf1a5a6ebc418d48e1e9e7f7bbf6b1f5b2bca535
Sergey <s.pakhirko@mail.ru>
2020-03-12 22:55:55 +0500

    Начальный проект

---
+++ src/main/kotlin/org/example/RunApp.kt
@@ -0,0 +1,13 @@
+package org.example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class caseCup
+
+
+fun main(args: Array<String>) {
+    runApplication<caseCup>(*args)
+
+}
